1aab3ca17659b6cff8e2eea77601a803d095be60
marktai <taifighterm@gmail.com>
2015-10-21 23:39:41 -0700

    added compression and decompression

--- src/dbinterface/main.go
+++ src/dbinterface/main.go
@@ -6,7 +6,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	// "gopkg.in/mgo.v2"
 	// "gopkg.in/mgo.v2/bson"
+	"fmt"
 	"log"
+)
+
+var (
+	db *sql.DB
 )
 
 type Person struct {
@@ -15,26 +20,26 @@ type Person struct {
 }
 
 type Game struct {
-	ID         int
-	comprBoard []int //0 - 4^10-1
-	Turn       int   // 0-19
-	Players    []int
+	ID         uint
+	Players    [2]uint
+	Turn       uint    // 0-19
+	comprBoard [9]uint //0 - 4^10-1
 }
 
 type dbgame struct {
-	gameid  int
-	player0 int
-	player1 int
-	turn    int
-	box0    int
-	box1    int
-	box2    int
-	box3    int
-	box4    int
-	box5    int
-	box6    int
-	box7    int
-	box8    int
+	gameid  uint
+	player0 uint
+	player1 uint
+	turn    uint
+	box0    uint
+	box1    uint
+	box2    uint
+	box3    uint
+	box4    uint
+	box5    uint
+	box6    uint
+	box7    uint
+	box8    uint
 }
 
 // Turn
@@ -42,44 +47,124 @@ type dbgame struct {
 // _0-8 -> box 0-8, 9 -> anywhere
 
 type Box struct {
-	Squares []int
-	Owned   int
+	Owned   uint
+	Squares [9]uint
 }
 
 type Board struct {
-	Boxes []Box
+	Boxes [9]*Box
 }
 
-func (g dbgame) game() Game {
-	players := []int{g.player0, g.player1}
-	comprBoard := []int{g.box0, g.box1, g.box2, g.box3, g.box4, g.box5, g.box6, g.box7, g.box8}
-	return Game{g.gameid, comprBoard, g.turn, players}
+func (g dbgame) game() *Game {
+	players := [2]uint{g.player0, g.player1}
+	comprBoard := [9]uint{g.box0, g.box1, g.box2, g.box3, g.box4, g.box5, g.box6, g.box7, g.box8}
+	return &Game{g.gameid, players, g.turn, comprBoard}
 }
 
-func (g Game) GetBoard() (Board, error) {
-	return Board{nil}, nil
+func (g *Game) GetBoard() (Board, error) {
+
+	var board Board
+
+	for i, comprBox := range g.comprBoard {
+		board.Boxes[i].Decompress(comprBox)
+	}
+	return board, nil
 }
 
-func Tester() {
-	db, err := sql.Open("mysql",
+func (b *Box) Decompress(compressed uint) error {
+
+	i := 9
+	for i >= 0 {
+		num := compressed & 0x03
+		if i == 9 {
+			b.Owned = num
+		} else {
+			b.Squares[i] = num
+		}
+		compressed >>= 2
+		i--
+	}
+	log.Println(b.Squares)
+	return nil
+}
+
+func (b *Box) Compress() uint {
+
+	var total uint
+
+	for _, square := range b.Squares {
+		total += square
+		total *= 4
+	}
+	total += b.Owned
+
+	return total
+}
+
+func (b *Box) Print() {
+
+	out := "\n"
+
+	for i := 0; i < 3; i++ {
+		if i != 0 {
+			out += fmt.Sprintln("---------")
+		}
+
+		out += fmt.Sprintf("%d | %d | %d\n", b.Squares[3*i], b.Squares[3*i+1], b.Squares[3*i+2])
+	}
+	// 	1 | 2 | 1
+	// 	---------
+	// 	2 | 1 | 2
+	// 	---------
+	// 	0 | 2 | 3
+	log.Println(out)
+}
+
+func GetGame(id uint) (*Game, error) {
+	err := db.Ping()
+	if err != nil {
+		// do something here
+		return nil, err
+	}
+
+	var game dbgame
+
+	err = db.QueryRow("select * from games where gameid=?", id).Scan(&game.gameid, &game.player0, &game.player1, &game.turn, &game.box0, &game.box1, &game.box2, &game.box3, &game.box4, &game.box5, &game.box6, &game.box7, &game.box8)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return game.game(), nil
+}
+
+func Server() {
+	var err error
+	db, err = sql.Open("mysql",
 		"root:@tcp(127.0.0.1:3306)/TT2")
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	err = db.Ping()
+	game, err := GetGame(0)
 	if err != nil {
-		// do something here
-		log.Fatal(err)
+		log.Println(err)
 	}
 
-	var id int
-	err = db.QueryRow("select gameid from games").Scan(&id)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(id)
+	log.Println(game)
+	// game.GetBoard()
+
+	b := &Box{0, [9]uint{0, 1, 1, 0, 2, 2, 2, 1, 0}}
+	b.Print()
+	compressed := b.Compress()
+	log.Printf("Compressed is %d\n", compressed)
+
+	c := &Box{}
+	c.Decompress(compressed)
+	c.Print()
+}
+
+func Tester() {
 }
 
 // func Tester() {
--- src/main/main.go
+++ src/main/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	dbinterface.Tester()
+	dbinterface.Server()
 }
